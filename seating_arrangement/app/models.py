7d73500ed49514f49a7c30089a094ae287ff26b8
jishnu <jishnub222@gmail.com>
2025-08-06 14:23:41 +0530

    add models

--- seating_arrangement/app/models.py
+++ seating_arrangement/app/models.py
@@ -1,3 +1,76 @@
 from django.db import models
 
 # Create your models here.
+
+
+#student tables
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    roll_number = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+#invigilator
+
+class Invigilator(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+#exam details
+
+
+class Exam(models.Model):
+    subject_code = models.CharField(max_length=20)
+    subject_name = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    duration = models.DurationField()
+
+    def __str__(self):
+        return self.subject_name
+
+#room info
+
+class Room(models.Model):
+    room_number = models.CharField(max_length=20, unique=True)
+    capacity = models.PositiveIntegerField()
+    supervisor = models.ForeignKey('Invigilator', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.room_number
+
+
+#seating info
+
+class Seating(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    seat_number = models.PositiveIntegerField()
