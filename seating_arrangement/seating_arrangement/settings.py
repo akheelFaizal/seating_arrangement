20fed44450e6f89a94d07293538269a103db0d5b
akheelFaizal <akheelfaizal888@gmail.com>
2025-08-06 18:18:54 +0530

    dashboard admin

--- seating_arrangement/seating_arrangement/settings.py
+++ seating_arrangement/seating_arrangement/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
 ]
+
+TAILWIND_APP_NAME = "theme"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
