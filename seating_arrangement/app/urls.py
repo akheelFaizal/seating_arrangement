2cbb44aff6815e66918330527e44608d92ef8d31
jishnu <jishnub222@gmail.com>
2025-08-07 15:59:52 +0530

    studentlogin page added

--- seating_arrangement/app/urls.py
+++ seating_arrangement/app/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('studentlogin',views.StudentLogin)
 ]
--- seating_arrangement/app/views.py
+++ seating_arrangement/app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'p1.html')
+
+
+def StudentLogin(request):
+    return render(request, 'studentLogin.html')
