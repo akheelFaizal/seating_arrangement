191cea8ab7aece643991c7d662fa9b0b7b25addf
akheelFaizal <akheelfaizal888@gmail.com>
2025-08-08 22:37:55 +0530

    moved admin and student pages to respective folders

--- seating_arrangement/app/views.py
+++ seating_arrangement/app/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, redirect
 
 
 def index(request):
-      return render(request, 'Admin.html')
+      return render(request, 'admin/Admin.html')
 
 
 def StudentLogin(request):
-    return render(request, 'studentLogin.html')
+    return render(request, 'student/studentLogin.html')
 
   
 
